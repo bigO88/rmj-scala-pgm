b7e032d6c77599cb0b35428f30d61702064e8037
Raj janwa <rajjanwa@gmail.com>
2017-12-23 23:53:05 +0530

    Added first actor program

---
+++ src/com/rmj/akka/example/AkkaRunner.scala
@@ -0,0 +1,31 @@
+package com.rmj.akka.example
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+object AkkaRunner extends App {
+  
+  class AkkaFisrtActor extends Actor {
+    def receive = {
+      case msg:String=> println("The actor recieved the msg:"+msg)
+      case (x:Int,y:Int) =>println("The multiplication of two number: "+ multiply(x,y)) 
+    }
+    
+    def multiply(x:Int,y:Int) = x*y
+  }
+  
+  val props=Props[AkkaFisrtActor]
+  
+  val name="FirstActor"  
+  
+  val system=ActorSystem()
+  
+  val actor=system.actorOf(props,name)
+  
+  actor !"This is the first actor"
+  actor ! (22,3)
+  
+  
+  
+}
